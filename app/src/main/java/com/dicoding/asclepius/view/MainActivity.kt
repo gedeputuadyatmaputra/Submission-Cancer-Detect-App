108a047e6c605ac23cbef35859afce6839258e8c
Gede Putu Adyatma Putra <gedeputu406@gmail.com>
2024-04-04 20:54:13 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.dicoding.asclepius.view
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import com.dicoding.asclepius.R
+import com.dicoding.asclepius.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var currentImageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.analyzeButton.setOnClickListener {
+            currentImageUri?.let {
+                analyzeImage()
+            } ?: run {
+                showToast(getString(R.string.image_classifier_failed))
+            }
+        }
+    }
+
+    private fun startGallery() {
+        // TODO: Mendapatkan gambar dari Gallery.
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("photoPicker", "No media selected")
+            showToast(getString(R.string.unselect_image))
+        }
+    }
+
+    private fun showImage() {
+        // TODO: Menampilkan gambar sesuai Gallery yang dipilih.
+        currentImageUri?.let {
+            Log.d("photoPicker", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
+    }
+
+    private fun analyzeImage() {
+        // TODO: Menganalisa gambar yang berhasil ditampilkan.
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra(ResultActivity.EXTRA_IMAGE_URI, currentImageUri.toString())
+        startActivity(intent)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
